25856c32116cefc6c355d4ed00ade7226a6418c6
sukrukiymaci <42950648+sukrukiymaci@users.noreply.github.com>
2020-04-22 04:42:19 -0600

    Create temp.java

---
+++ temp.java
@@ -0,0 +1,21 @@
+//temp explanation for my friend
+public class HelloWorld{
+
+     public static void main(String []args){
+        
+        
+        
+        for(int i = 0; i< 100 ; i++ ){
+        
+            
+            System.out.println(i + " ");
+             
+            if(i % 20 == 0 && i != 0    ){
+                for (int j = 0 ; j<10 ; j ++){
+                System.out.print(0 + " " );
+                }
+            }
+        
+        }
+    }
+}
