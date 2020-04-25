4b3abe1a50d83ce89980fb667da8ecc932d3da94
sukrukiymaci <42950648+sukrukiymaci@users.noreply.github.com>
2020-04-24 18:27:30 -0600

    Update Day_6.java

--- Day_6.java
+++ Day_6.java
@@ -1,3 +1,26 @@
+/*
+
+Given an array of strings, group anagrams together.
+
+Example:
+
+Input: ["eat", "tea", "tan", "ate", "nat", "bat"],
+Output:
+[
+  ["ate","eat","tea"],
+  ["nat","tan"],
+  ["bat"]
+]
+Note:
+
+All inputs will be in lowercase.
+The order of your output does not matter
+
+
+*/
+
+
+//This is the first solution that I implemented but timeouts for the 100.th case out of 101
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
         
@@ -30,9 +53,7 @@ class Solution {
                     }
                     
                 }
-                
-
-                
+            
             }
             
             if(list3.isEmpty()==true){
@@ -46,9 +67,32 @@ class Solution {
         return list;
 
         }
-        
+       
+    }
 
+
+//This is the second solution that I found implemented by someone else and shared to public which does not timeout
+
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
         
-        
+        if (strs.length == 0) {
+            return new ArrayList();
+        }
+        Map<String, List> ans = new HashMap<String, List>();
+        for (String s : strs) {
+            char[] ca = s.toCharArray();
+            Arrays.sort(ca);
+            String key = String.valueOf(ca);
+            if (!ans.containsKey(key)) {
+                ans.put(key, new ArrayList());
+            }
+            ans.get(key).add(s);
+        }
+        return new ArrayList(ans.values());
+
+        }        
         
     }
+
+
