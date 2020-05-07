cd17b27cc33680f2e58618d715e783f26b3a11a0
sukrukiymaci <42950648+sukrukiymaci@users.noreply.github.com>
2020-05-07 15:08:39 -0600

    Update day_16.java

--- day_16.java
+++ day_16.java
@@ -1,8 +1,22 @@
+/*
+
+Given a string containing only three types of characters: '(', ')' and '*', write a function to check whether this string is valid. We define the validity of a string by these rules:
+
+Any left parenthesis '(' must have a corresponding right parenthesis ')'.
+Any right parenthesis ')' must have a corresponding left parenthesis '('.
+Left parenthesis '(' must go before the corresponding right parenthesis ')'.
+'*' could be treated as a single right parenthesis ')' or a single left parenthesis '(' or an empty string.
+An empty string is also valid.
+
+
+
+*/
+
 class Solution {
     public boolean checkValidString(String s) {
          int a = 0;
         
-        // Copy character by character into array 
+
         for (int i = 0; i < s.length(); i++) {   
             char c = s.charAt(i); 
             if(c == '*' || c == '(')a++;
