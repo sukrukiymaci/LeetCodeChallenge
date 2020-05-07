7972f71c8cedc0407142d2c4734cc332e110a3f7
sukrukiymaci <42950648+sukrukiymaci@users.noreply.github.com>
2020-05-07 15:37:41 -0600

    Update day_16.java

--- day_16.java
+++ day_16.java
@@ -12,17 +12,21 @@ An empty string is also valid.
 
 */
 
+// I used a very different approach for this question
+//There was a trick in the question which '*' can be used as ')' or '(' if it is needed else does not change the balanced paranthesis
 class Solution {
     public boolean checkValidString(String s) {
          int a = 0;
         
-
+        // First we start from the beginning of the string and check if there exists extra paranthesis and the extra one is ')'
         for (int i = 0; i < s.length(); i++) {   
             char c = s.charAt(i); 
             if(c == '*' || c == '(')a++;
             else if(c == ')' && a>0 ) a--;
             else if(c == ')')return false;
         }
+        
+        // Starts from the last element and check if there is extra '(' paranthesis
         a = 0 ;
         for (int i = s.length()-1; i >= 0 ; i--) {   
             char c = s.charAt(i); 
