9ff33ef3123278f6258c602711cb5d8ad74624f4
sukrukiymaci <42950648+sukrukiymaci@users.noreply.github.com>
2020-05-28 18:42:51 -0600

    Update Day_22.java

--- Day_22.java
+++ Day_22.java
@@ -1,3 +1,19 @@
+/*
+Given a range [m, n] where 0 <= m <= n <= 2147483647, return the bitwise AND of all numbers in this range, inclusive.
+
+Example 1:
+
+Input: [5,7]
+Output: 4
+Example 2:
+
+Input: [0,1]
+Output: 0
+
+*/
+
+
+//If case in the loop is critical here because if we dont put this case it exceeds the time limit. Every time number multiplies by two and and value becomes 0
 class Solution {
     public int rangeBitwiseAnd(int m, int n) {
         int k = m++;
